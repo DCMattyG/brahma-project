9f1d0839ef3afde73f3e6b80f331c0c67e04dadd
dcmattyg <mgarrett0402@gmail.com>
2020-01-14 23:33:28 -0800

    updated docker build details

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-const MONGO_ADDR = process.env.MONGO_URL || 'mongodb://localhost';
+const MONGO_ADDR = process.env.MONGO_URL || 'localhost';
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
-const MONGO_URL = MONGO_ADDR + ':' + MONGO_PORT;
+const MONGO_URL = 'mongodb://' + MONGO_ADDR + ':' + MONGO_PORT;
 
 // CORS Settings
 var originsWhitelist = [
