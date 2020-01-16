805c642c057105bc025f4624d8c70a4786fe82b2
dcmattyg <mgarrett0402@gmail.com>
2020-01-15 23:02:39 -0800

    added compose feature to assist script

--- brahma-server.sh
+++ brahma-server.sh
@@ -26,6 +26,12 @@ elif [ "$1" == "--build" ]; then
   echo ""
   echo "Building Docker Container..."
   docker build -t brahma .
+elif [ "$1" == "--compose" ]; then
+  echo "Building Angular DIST..."
+  ng build --prod
+  echo ""
+  echo "Building Docker Container..."
+  docker-compose up
 else
   echo ""
   echo "Usage: brahma-server [--dev] | [--prod] | [--build]"
@@ -33,9 +39,10 @@ else
   echo "Brahma Server Script"
   echo ""
   echo "Optional Arguments:"
-  echo "--dev     Run Server in Development Mode"
-  echo "--prod    Run Server in Production Mode"
-  echo "--build   Build Docker Container for Brahma Service"
+  echo "--dev       Run Server in Development Mode"
+  echo "--prod      Run Server in Production Mode"
+  echo "--build     Build Docker Container for Brahma Service"
+  echo "--compose   Bring Full Brahma Service Online w/ docker-compose"
   echo ""
   exit 0
 fi
