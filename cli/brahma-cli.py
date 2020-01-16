eed618be200a24e44c7809eb04a4dbe60dd587f6
dcmattyg <mgarrett0402@gmail.com>
2020-01-16 01:29:53 -0800

    removed cli debug messaging

--- cli/brahma-cli.py
+++ cli/brahma-cli.py
@@ -28,8 +28,6 @@ if(BRAHMA_PORT != None):
 
 SERVER_URL = BRAHMA_URL + '/api/fabric'
 
-print("BRAHMA ADDRESS: " + BRAHMA_URL)
-
 IP_ADDR = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 HOST_FQDN = "^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$"
 
