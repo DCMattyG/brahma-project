690767789f508b80561a821b14d9674446dfd89e
dcmattyg <mgarrett0402@gmail.com>
2020-01-16 22:13:43 -0800

    created main function in brahma cli

--- brahma-cli/brahma_cli/brahma.py
+++ brahma-cli/brahma_cli/brahma.py
@@ -82,114 +82,127 @@ class FabricTopology():
   def toJSON(self):
     return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
-urllib3.disable_warnings()
+def main():
+  urllib3.disable_warnings()
 
-arg_parser = argparse.ArgumentParser(prog='brahma', description='Brahma CLI Utility')
-arg_parser.version = '1.0'
-arg_parser.add_argument('-n', dest='new', action='store_true', help='New Fabric', required=True)
-arg_parser.add_argument('-v', action='version', help='Show Version')
-args = arg_parser.parse_args()
+  args = parseArgs()
 
-print('')
+  if(args.new):
+    newFabric()
 
-apic_addr = six.moves.input("APIC IP Address/FQDN: ")
+def parseArgs():
+  arg_parser = argparse.ArgumentParser(prog='brahma', description='Brahma CLI Utility')
+  arg_parser.version = '1.0'
+  arg_parser.add_argument('-n', dest='new', action='store_true', help='New Fabric', required=True)
+  arg_parser.add_argument('-v', action='version', help='Show Version')
+  args = arg_parser.parse_args()
 
-is_ip = re.match(IP_ADDR, apic_addr)
-is_host = re.match(HOST_FQDN, apic_addr)
+  return args
 
-if (is_ip):
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.settimeout(0.5)
+def newFabric():
+  print('')
 
-  try:
-    s.connect((apic_addr,443)) 
-  except Exception as e:
-    print("APIC IP Address unreachable!")
+  apic_addr = six.moves.input("APIC IP Address/FQDN: ")
+
+  is_ip = re.match(IP_ADDR, apic_addr)
+  is_host = re.match(HOST_FQDN, apic_addr)
+
+  if (is_ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+
+    try:
+      s.connect((apic_addr,443)) 
+    except:
+      print("APIC IP Address unreachable!")
+      sys.exit()
+
+  if (is_host):
+    try:
+      socket.gethostbyname(apic_addr)
+    except socket.gaierror:
+      print("APIC FQDN unreachable!")
+      sys.exit()
+
+  if not is_ip and not is_host:
+    print("Not a valid IP Address or FQDN!")
     sys.exit()
 
-if (is_host):
+  apic_user = six.moves.input("APIC Username: ")
+  apic_pass = getpass.getpass(prompt='APIC Password: ', stream=None) 
+
+  apic_url = "https://" + apic_addr
+
+  session = LoginSession(apic_url, apic_user, apic_pass)
+  moDir = MoDirectory(session)
+
   try:
-    socket.gethostbyname(apic_addr)
-  except socket.gaierror:
-    print("APIC FQDN unreachable!")
+    moDir.login()
+  except requests.exceptions.RequestException:
+    print("Cannot connect to APIC!")
     sys.exit()
 
-if not is_ip and not is_host:
-  print("Not a valid IP Address or FQDN!")
-  sys.exit()
+  cq = ClassQuery('fabricNode')
 
-apic_user = six.moves.input("APIC Username: ")
-apic_pass = getpass.getpass(prompt='APIC Password: ', stream=None) 
+  nodes = moDir.query(cq)
 
-apic_url = "https://" + apic_addr
+  fNodes = FabricTopology()
 
-session = LoginSession(apic_url, apic_user, apic_pass)
-moDir = MoDirectory(session)
+  for node in nodes:
+    if(node.role == 'spine' or node.role=='leaf' or node.role=='controller'):
+      node_name = str(node.name)
+      node_real = str(node.rn)
+      node_role = str(node.role)
+      node_serial = str(node.serial)
+      node_model = str(node.model)
+      node_id = str(node.id)
+      fNodes.add_node(node_name, node_real, node_role, node_serial, node_model, node_id)
 
-try:
-  moDir.login()
-except requests.exceptions.RequestException:
-  print("Cannot connect to APIC!")
-  sys.exit()
+  # print('') 
 
-cq = ClassQuery('fabricNode')
+  for fNode in fNodes.get_nodes():
+    # print('{}({}) -> Role: {}'.format(fNode.name, fNode.serial, fNode.role))
+    # print('-----')
+    nodeDn = 'topology/pod-1/' + fNode.rn + '/sys'
+    intfs = moDir.lookupByClass("l1PhysIf", parentDn=nodeDn) 
+    for intf in intfs:
+      if(intf.usage == 'fabric') and (intf.switchingSt == 'enabled'):
+        int_id = str(intf.id)
+        lldpDn = nodeDn + '/lldp/inst/if-[' + int_id + ']'
+        lldpns = moDir.lookupByClass("lldpAdjEp", parentDn=lldpDn)
 
-nodes = moDir.query(cq)
+        for lldpn in lldpns:
+          dest_name = lldpn.sysName
+          port_desc = lldpn.portDesc
+          dest_length = len(port_desc) - 1
+          left_bracket = port_desc.find('[') - dest_length
+          dest_port = port_desc[left_bracket:-1]
+          fNode.add_connection(int_id, dest_name, dest_port)
+          # print('{} -> {}({})'.format(int_id, dest_name, dest_port))
+    # print('')
 
-fNodes = FabricTopology()
-
-for node in nodes:
-  if(node.role == 'spine' or node.role=='leaf' or node.role=='controller'):
-    node_name = str(node.name)
-    node_real = str(node.rn)
-    node_role = str(node.role)
-    node_serial = str(node.serial)
-    node_model = str(node.model)
-    node_id = str(node.id)
-    fNodes.add_node(node_name, node_real, node_role, node_serial, node_model, node_id)
-
-# print('') 
-
-for fNode in fNodes.get_nodes():
-  # print('{}({}) -> Role: {}'.format(fNode.name, fNode.serial, fNode.role))
-  # print('-----')
-  nodeDn = 'topology/pod-1/' + fNode.rn + '/sys'
-  intfs = moDir.lookupByClass("l1PhysIf", parentDn=nodeDn) 
-  for intf in intfs:
-    if(intf.usage == 'fabric') and (intf.switchingSt == 'enabled'):
-      int_id = str(intf.id)
-      lldpDn = nodeDn + '/lldp/inst/if-[' + int_id + ']'
-      lldpns = moDir.lookupByClass("lldpAdjEp", parentDn=lldpDn)
-
-      for lldpn in lldpns:
-        dest_name = lldpn.sysName
-        port_desc = lldpn.portDesc
-        dest_length = len(port_desc) - 1
-        left_bracket = port_desc.find('[') - dest_length
-        dest_port = port_desc[left_bracket:-1]
-        fNode.add_connection(int_id, dest_name, dest_port)
-        # print('{} -> {}({})'.format(int_id, dest_name, dest_port))
+  # print('--------------------------')  
   # print('')
 
-# print('--------------------------')  
-# print('')
+  node_json = fNodes.toJSON()
 
-node_json = fNodes.toJSON()
+  # print(node_json)
 
-# print(node_json)
+  json_data = json.loads(node_json)
 
-json_data = json.loads(node_json)
+  req = requests.post(url = SERVER_URL, json = json_data)
+  # print(req.status_code)
 
-req = requests.post(url = SERVER_URL, json = json_data)
-# print(req.status_code)
+  resp_json = json.loads(req.text)
+  # print(json.dumps(resp_json, indent=4, sort_keys=False))
 
-resp_json = json.loads(req.text)
-# print(json.dumps(resp_json, indent=4, sort_keys=False))
+  print('')
+  print('Fabric Uploaded!')
+  print('')
+  print('Token: {}'.format(resp_json['token']))
+  print('')
+  print('Please navigate to {} and use the above token.'.format(BRAHMA_URL))
+  print('')
 
-print('')
-print('Fabric Uploaded!')
-print('')
-print('Token: {}'.format(resp_json['token']))
-print('')
-print('Please navigate to {} and use the above token.'.format(BRAHMA_URL))
-print('')
+if __name__ == '__main__':
+  main()
